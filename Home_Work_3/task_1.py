820a1416d403a724ca66f7fd6835e26a59ce6899
Igor Zhidkov <stork32@mail.ru>
2023-02-26 18:04:06 +0300

    task_1 re

--- Home_Work_3/task_1.py
+++ Home_Work_3/task_1.py
@@ -9,17 +9,19 @@
 
 import random
 
-listSize = int(input('Введите размер списка: '))
-numberSearch = int(input('Введите число которое нужно найти: '))
-listOne = []
-countNumbers = 0
+list_size = int(input('Введите размер списка: '))
+number_search = int(input('Введите число которое нужно найти: '))
+list_one = []
+count_numbers = 0
 
-for i in range(listSize):
-    listOne.append(random.randint(1, 10))
-    if listOne[i] == numberSearch:
-        countNumbers += 1
+for i in range(list_size):
+    list_one.append(random.randint(1, 10))
+    
+for j in list_one:
+    if j == number_search:
+        count_numbers += 1
         
-print(*listOne)
+print(*list_one)
 print()
-print(countNumbers)
+print(count_numbers)
 
