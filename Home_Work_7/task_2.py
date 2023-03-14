f69663fa1774bae60c40eaa75d144ab8539231bc
Igor_Zhidkov <stork32@mail.ru>
2023-03-14 11:46:07 +0300

    Task_2 HW_8 re

--- Home_Work_7/task_2.py
+++ Home_Work_7/task_2.py
@@ -5,15 +5,12 @@ num_columns = int(input('Введите количество столбцов: '
 def print_operation_table(f, num_rows, num_columns):
     x = int(input('Введите номер строки: '))
     y = int(input('Введите номер столбца: '))
-    lst_1 = []
     for i in range(1, num_rows + 1):
         lst_2 = []
         for j in range(1, num_columns + 1):
-            lst_2.append(i * j)
-        lst_1.append(lst_2)
+            lst_2.append(f(i,j))
+        print(*lst_2)
 
-    for i in lst_1:
-        print(*i)
 
     print(f'Ваше число: {f(x,y)}')
 
